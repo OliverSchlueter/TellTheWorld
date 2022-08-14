5e55ca3815da2be9496bfb122964a5d05f8f5fc1
OliverSchlueter <oliverhd1303@gmail.com>
2022-08-14 12:09:47 +0200

    Setup spring boot for the API

---
+++ Code/API/src/main/java/de/oliver/telltheworldapi/TellTheWorldApiApplication.java
@@ -0,0 +1,13 @@
+package de.oliver.telltheworldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TellTheWorldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TellTheWorldApiApplication.class, args);
+	}
+
+}
---
+++ Code/API/src/test/java/de/oliver/telltheworldapi/TellTheWorldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.oliver.telltheworldapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TellTheWorldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
