13509adca067f11664e118166585c6b2a44e9571
OliverSchlueter <oliverhd1303@gmail.com>
2022-08-14 11:42:19 +0200

    Delete old python API

--- Code/Backend/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-BASE = "http://localhost:8080/api/v1/"
-
-# "message_id", "timestamp", "user_tag", "content"
-response = requests.put(BASE + "message/msgasfasfasf/", {"message_id": "12344242141", "timestamp": "5464651", "user_tag": "Oliver#80273", "content": "POG POGPO GOPGOO :beers:"})
-print(response.json())
-
